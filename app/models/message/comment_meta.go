84917af3d4d59264d02ce9164315d0adf9344899
xinshuaifeng <xinshuaifeng@big.one>
2022-01-14 21:40:14 +0800

    blank username

--- app/models/message/comment_meta.go
+++ app/models/message/comment_meta.go
@@ -8,7 +8,7 @@ type CommentMeta struct {
 	CommentID            primitive.ObjectID `bson:"comment_id,omitempty"`
 	Content              string             `bson:"content,omitempty"`
 	ParentContent        string             `bson:"parent_content,omitempty"`
-	ParentUserName       string             `bson:"parent_user_name,omitempty"`
+	ParentUsername       string             `bson:"parent_username,omitempty"`
 	StatusContentSummary string             `bson:"status_content_summary,omitempty"`
 	StatusImagePath      string             `bson:"status_image_path,omitempty"`
 	StatusImageURL       string             `bson:"-"`
